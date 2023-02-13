bfcd6f479b81e90c1ef35a68ff0df0903d1ead3e
Matias Asensi <mati.asensi@gmail.com>
2023-02-13 15:01:28 -0300

    dd

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -6,3 +6,4 @@ export const ItemList = ({products}) => {
     </>
   )
 }
+export default ItemList;
--- src/components/Itemlistcontainer/Itemlistcontainer.jsx
+++ src/components/Itemlistcontainer/Itemlistcontainer.jsx
@@ -1,22 +1,38 @@
 import {useState,useEffect} from 'react'
 import { ItemList } from '../ItemList/ItemList'
+import { useParams } from 'react-router-dom'
 
 export const ItemlistContainer = () => {
-    const [productos, setProductos] = useState([])
+    const [productos, setProductos] = useState([]);
+    const {idCategoria}= useParams()
     useEffect(() => {
-        fetch('../json/productos.json')
-        .then(response => response.json())
-        .then(products => {
-            console.log(products)
-            const productsList = ItemList({products})
-            console.log(productsList)
-            setProductos(productsList)
-        })
-    }, [])
-    return (
-        <div className='row'>
-            {productos}
-        </div>
-    )
+        if(idCategoria) {
+            fetch('../json/productos.json')
+            .then(response => response.json())
+            .then(items => {
+                const products = items.filter(prod => prod.idCategoria === parseInt(idCategoria))
+                const productsList = ItemList({products}) 
+                console.log(productsList)
+                setProductos(productsList)
+            })
+        } else {
+            fetch('./json/productos.json')
+            .then(response => response.json())
+            .then(products => {
+                console.log(products)
+                const productsList = ItemList({products}) 
+                console.log(productsList)
+                setProductos(productsList)
+            })
+        }
+        
+    }, [idCategoria])
+return (
+    <div className='row cardProductos'>
+        {productos}
+    </div>
+)
 }
+
 export default ItemlistContainer;
+
